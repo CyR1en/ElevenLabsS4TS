cbecac02816212bb749932c8b1fc1e0d6dabb810
cyr1en <cyrien3519@gmail.com>
2023-04-09 12:40:45 -0600

    Faster whisper config

--- whisper.py
+++ whisper.py
@@ -1,10 +1,26 @@
 import torch
-from transformers import pipeline
+import soundfile as sf
+from transformers import WhisperProcessor, WhisperForConditionalGeneration, WhisperTokenizerFast
 
-dev_id = 0 if torch.cuda.is_available() else -1
-print(f'Using device: {dev_id}')
-whisper = pipeline('automatic-speech-recognition', model='openai/whisper-medium', device=dev_id)
+# load model and processor
+tokenizer = WhisperTokenizerFast.from_pretrained('openai/whisper-base')
+processor = WhisperProcessor.from_pretrained('openai/whisper-base', tokenizer=tokenizer)
+model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-base")
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print('Using device: ', device)
+model.to(device)
+
+model.config.forced_decoder_ids = None
+
+
+def transcribe(file_name: str) -> str:
+    audio, sample_rate = sf.read(file_name)
+
+    input_features = processor(audio, sampling_rate=sample_rate, return_tensors="pt").input_features
+    predicted_ids = model.generate(input_features, max_length=1000)
+    transcription: str = processor.batch_decode(predicted_ids, skip_special_tokens=True)[0]
+    return transcription.strip()
 
 def whisper_transcribe(file_name: str) -> str:
     text = whisper(file_name)
