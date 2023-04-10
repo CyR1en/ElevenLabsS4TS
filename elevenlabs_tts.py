61b29c68a659295da88c8461ca20e32f00dcf4c7
Ethan Bacurio <ethan.bacurio@ucdenver.edu>
2023-04-10 01:24:00 -0600

    Remove redundant code

--- elevenlabs_tts.py
+++ elevenlabs_tts.py
@@ -9,8 +9,6 @@ from configuration import ConfigFile, ConfigNode
 
 class ElevenLabsTTS:
     def __init__(self, config: ConfigFile):
-        key = config.get(ConfigNode.API_KEY)
-        print(key)
         self.user = ElevenLabsUser(config.get(ConfigNode.API_KEY))
 
     def get_voices(self) -> list:
@@ -22,13 +20,3 @@ class ElevenLabsTTS:
         voice = self.user.get_voices_by_name(voice)[0]
         data = voice.generate_audio_bytes(text, 0.7, 0.7)
         save_bytes_to_path("elevenlabs.wav", data)
-
-    def play_sound_os(self, sound: str) -> None:
-        """Play a sound"""
-        sys = platform.system()
-        if sys == 'Windows':
-            os.system(sound)
-        if sys == 'Mac':
-            os.system(f'afplay {sound}')
-        if sys == 'Linux':
-            os.system(f'mpg123{sound}')
